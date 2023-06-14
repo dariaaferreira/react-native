393608779d43256ab6a20b104d2c4c35b2e18d23
Daria Tkachenko <113775461+dariaaferreira@users.noreply.github.com>
2023-06-15 01:14:13 +0300

    create CreatePostsScreen

---
+++ Screens/CreatePostsScreen.jsx
@@ -0,0 +1,126 @@
+import React from "react";
+import { KeyboardAvoidingView, 
+  View, 
+  ImageBackground,
+  TextInput, 
+  TouchableOpacity, 
+  Text, 
+  StyleSheet, 
+  TouchableWithoutFeedback,
+  Keyboard } from 'react-native';
+import { Header } from "../Components/Header";
+import EvilIcons from 'react-native-vector-icons/EvilIcons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
+const CreatePostsScreen = () => (
+  <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+      <Header pageTitle="Створити публікацію" />
+        <KeyboardAvoidingView
+          behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+          style={{ flex: 1 }}
+        >
+
+        <ImageBackground style={styles.postImage}>
+          <View style={styles.icon}>
+            <FontAwesome
+              name="camera"
+              size={24}
+              style={{ color: "#BDBDBD", opacity: 0.5 }}
+            />
+          </View>
+        </ImageBackground>
+        
+        <Text style={styles.buttonText}>Завантажте фото</Text>
+
+        <View style={styles.containerInput}>
+          <TextInput
+            style={styles.input}
+            placeholder="Назва..."
+          />
+              <EvilIcons
+              name="location"
+              size={24}
+              style={{
+                position: "absolute",
+                top: 80,
+                left: 0,
+                color: "#BDBDBD",
+                opacity: 0.5,
+              }}
+            />
+          <TextInput
+            style={[styles.input, styles.inputIcon, styles.lastChildInput]}
+            placeholder="Місцевість..."
+          />
+        </View>
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Опубліковати</Text>
+        </TouchableOpacity>
+
+        </KeyboardAvoidingView>
+      </View>
+    </TouchableWithoutFeedback>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    minHeight: 720,
+    paddingTop: 32,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  postImage: {
+    width: 343,
+    height: 240,
+    backgroundColor: '#E8E8E8',
+    borderRadius: 8,
+    marginTop: 32,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  icon: {
+    width: 60,
+    height: 60,
+    borderRadius: 100,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  containerInput: {
+    position: 'relative',
+    marginTop: 32,
+  },
+  input: {
+    width: 343,
+    height: 50,
+    borderColor: '#E8E8E8',
+    borderBottomWidth: 1,
+    marginBottom: 16,
+    paddingHorizontal: 8,
+  },
+  inputIcon: {
+    paddingHorizontal: 25,
+  },
+  lastChildInput: {
+    marginBottom: 43,
+  },
+  button: {
+    backgroundColor: '#F6F6F6',
+    width: 343,
+    alignItems: 'center',
+    padding: 16,
+    height: 51,
+    borderRadius: 100,
+    justifyContent: 'center',
+  },
+  buttonText: {
+    color: '#BDBDBD',
+    fontSize: 16,
+  },
+});
+
+export default CreatePostsScreen;
