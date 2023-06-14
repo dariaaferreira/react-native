3666153057c4f9a631d5fd307f5ba0a6fcd4c0fb
Daria Tkachenko <113775461+dariaaferreira@users.noreply.github.com>
2023-06-15 01:14:59 +0300

    create PostsScreen

---
+++ Screens/PostsScreen.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+import Avatar from '../Images/Avatar.jpg';
+import { Header } from "../Components/Header";
+
+const PostsScreen = () => (
+    <View style={styles.container}>
+    <Header pageTitle="Публікації" />
+
+    <View style={styles.user}>  
+        <Image
+          source={Avatar}
+          style={styles.avatar}
+        />
+
+        <View style={styles.description}> 
+          <Text style={styles.name}>Natali Romanova</Text>
+          <Text>email@example.com</Text>
+        </View>
+    </View>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    minHeight: 720,
+    paddingTop: 32,
+    backgroundColor: '#fff',
+  },
+  user: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: 32,
+    paddingLeft: 16,
+    paddingRight: 16,
+  },
+  avatar: {
+    width: 60,
+    height: 60,
+    borderRadius: 16, 
+  },
+  description: {
+    flexDirection: 'column',
+    marginLeft: 8,
+  },
+  name: {
+    fontSize: 13,
+    fontWeight: 700,
+    lineHeight: 15.23,
+  },
+});
+
+export default PostsScreen;
