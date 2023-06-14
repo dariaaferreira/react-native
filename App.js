da7a7ebe560d36e58786a60a195a0ede1a5f8b06
Daria Tkachenko <113775461+dariaaferreira@users.noreply.github.com>
2023-06-15 01:13:06 +0300

    added Home page

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import RegistrationScreen from './Screens/RegistrationScreen';
 import LoginScreen from './Screens/LoginScreen';
+import Home from "./Screens/Home";
 
 const MainStack = createStackNavigator();
 
@@ -18,6 +19,11 @@ export default function App() {
         <MainStack.Screen 
           name="Login" 
           component={LoginScreen}  
+          options={{ headerShown: false }} 
+        />
+        <MainStack.Screen 
+          name="Home"
+          component={Home}
           options={{ headerShown: false }} 
         />
       </MainStack.Navigator>
